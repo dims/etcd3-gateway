6199d5bec553a220f2e81e6c476bad9dae0e97b7
Davanum Srinivas <davanum@gmail.com>
2017-03-25 10:24:37 -0400

    Add tox targets that run tests with etcd

--- etcd3gw/tests/test_etcd3gw.py
+++ etcd3gw/tests/test_etcd3gw.py
@@ -17,11 +17,24 @@ test_etcd3-gateway
 Tests for `etcd3gw` module.
 """
 
+from testtools.testcase import unittest
+import urllib3
+
 from etcd3gw.client import Client
 from etcd3gw.tests import base
 
 
+def _is_etcd3_running():
+    try:
+        urllib3.PoolManager().request('GET', '127.0.0.1:2379')
+        return True
+    except urllib3.exceptions.HTTPError:
+        return False
+
+
 class TestEtcd3Gateway(base.TestCase):
+    @unittest.skipUnless(
+        _is_etcd3_running(), "etcd3 is not available")
     def test_something(self):
         client = Client()
         self.assertIsNotNone(client.status())
